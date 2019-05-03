6dd53506f34a6aae82f94fc81ba1dd855a3e946d
Niandubay <niandubay79@gmail.com>
2019-05-02 23:33:04 -0300

    9-02

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,19 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  var app = MaterialApp(
-    home: Scaffold(
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          print('Hi there!');
-        },
-      ),
-      appBar: AppBar(
-        title: Text('Lets see some images!'),
-      ),
-    ),
-  );
+  var app = ;
 
   runApp(app);
 }
