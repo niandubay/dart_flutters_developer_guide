9d8419c1e157c636eae54146cc577ae6ed9de1e4
Niandubay <niandubay79@gmail.com>
2019-05-02 22:38:49 -0300

    8-06

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -3,8 +3,11 @@
 import 'package:flutter/material.dart';
 
 // Define a 'main' function to run when our apps starts
-
-// Create a new text widget to show some text
-// on the screen
-
-// Take that widget and get it on the screen
+void main() {
+  // Create a new text widget to show some text
+  // on the screen
+  var app = MaterialApp(
+    home: Text('Hi there!'),
+  );
+  // Take that widget and get it on the screen
+}
