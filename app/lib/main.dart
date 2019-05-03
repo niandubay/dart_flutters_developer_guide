fbea34ff613d8ca53f4e932878ad4d56aec02242
Niandubay <niandubay79@gmail.com>
2019-05-02 22:19:30 -0300

    8-04

---
+++ app/lib/main.dart
@@ -0,0 +1,9 @@
+// I need to import a helper library
+// from flutter to get content on the screen
+
+// Define a 'main' function to run when our apps starts
+
+// Create a new text widget to show some text
+// on the screen
+
+// Take that widget and get it on the screen
