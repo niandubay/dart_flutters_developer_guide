a1ada3ea796102939e7ba7fa08ace1ffdf2bfcd1
Niandubay <niandubay79@gmail.com>
2019-05-06 22:58:14 -0300

    9-05

--- app/lib/src/app.dart
+++ app/lib/src/app.dart
@@ -1,9 +1,11 @@
 // Import flutter helper library
 import 'package:flutter/material.dart';
 
+class App extends StatefulWidget {}
+
 // Create a class that will be our custom widget
 // This class must extend the 'StatelessWidget' base class
-class App extends StatelessWidget {
+class AppState extends State<App> {
   //Must define a 'build' method that returns
   // the widgets that *this* widget will show
   Widget build(context) {
