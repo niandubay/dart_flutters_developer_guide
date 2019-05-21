d941d32bf986fe9fdfb4359dffad1fff8456aca6
Niandubay <niandubay79@gmail.com>
2019-05-20 23:08:57 -0300

    11-10

--- app/lib/src/app.dart
+++ app/lib/src/app.dart
@@ -1,6 +1,8 @@
 // Import flutter helper library
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+import 'dart:convert';
 
 class App extends StatefulWidget {
   @override
@@ -12,9 +14,11 @@ class App extends StatefulWidget {
 class AppState extends State<App> {
   int counter = 0;
 
-  void fetchImage() {
+  void fetchImage() async {
     counter++;
-    get('https://jsonplaceholder.typicode.com/photos/$counter');
+    var response =
+        await get('https://jsonplaceholder.typicode.com/photos/$counter');
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
   }
 
   Widget build(context) {
