ae3f1b97ce5a7def21e1baf76ee1f01c1e93749b
Niandubay <niandubay79@gmail.com>
2019-05-24 23:54:24 -0300

    12-15

--- login_stateful/lib/src/screens/login_screen.dart
+++ login_stateful/lib/src/screens/login_screen.dart
@@ -8,11 +8,14 @@ class LoginScreen extends StatefulWidget {
 }
 
 class LoginScreenState extends State<LoginScreen> {
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Form(
+        key: formKey,
         child: Column(
           children: <Widget>[
             emailField(),
@@ -53,7 +56,9 @@ class LoginScreenState extends State<LoginScreen> {
     return RaisedButton(
       color: Colors.blue,
       child: Text('Submit!'),
-      onPressed: () {},
+      onPressed: () {
+        formKey.currentState.reset();
+      },
     );
   }
 }
