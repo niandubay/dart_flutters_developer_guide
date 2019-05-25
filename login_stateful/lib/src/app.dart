46cce6f2210120c297fc8e2dc085e258b503d42a
Niandubay <niandubay79@gmail.com>
2019-05-24 21:42:08 -0300

    12-03

--- login_stateful/lib/src/app.dart
+++ login_stateful/lib/src/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Log Me In!',
