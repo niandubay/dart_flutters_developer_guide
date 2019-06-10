a7dfce3db51749733cfbc5a86d21e1c8f270bebc
Niandubay <niandubay79@gmail.com>
2019-06-10 20:49:55 -0300

    14-15

--- login_bloc/lib/src/blocs/bloc.dart
+++ login_bloc/lib/src/blocs/bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
+import 'validators.dart';
 
-class Bloc {
+class Bloc extends Object with Validators {
   final _email = StreamController<String>();
   final _password = StreamController<String>();
 
